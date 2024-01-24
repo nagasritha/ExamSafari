537e92a332a9ccf473ee2c376d3ecf52d591d9b2
nagasritha <erchandrakalachandrakala665@gmail.com>
2024-01-24 09:00:40 +0530

    removed all the console errors

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -54,13 +54,11 @@ function Services() {
         
       <div className="services-item relative h-64 transition-all duration-300 hover:bg-gradient-to-t hover:from-blue-500 hover:to-purple-500">
           <img className="h-[300px] w-auto rounded-[25px] p-2 service-image" src="./images/service1.png" alt="dummy-image"/>
-          <a href="" className='absolute top-1 ml-2 text-2xl font-semibold text-neutral-50 pt-6 pl-4'></a>
           <button className="services-item-btn">View More..</button>
       </div>
       
       <div className="services-item relative h-64 transition-all duration-300 hover:bg-gradient-to-t hover:from-blue-500 hover:to-purple-500">
           <img className=" h-[300px] w-auto rounded-[25px] p-2 service-image" src="./images/service2.png" alt="dummy-image"/>
-          <a href="" className='absolute top-1 ml-2 text-2xl font-semibold uppercase text-neutral-50 pt-6 pl-4'></a>
           <Link to='/accomadation'>
             <button className="services-item-btn">View More..</button>
           </Link>
@@ -69,7 +67,6 @@ function Services() {
       
       <div className="services-item relative h-64 transition-all duration-300 hover:bg-gradient-to-t hover:from-blue-500 hover:to-purple-500">
           <img className="h-[300px] w-auto rounded-[25px] p-2 service-image" src="./images/service3.png" alt="dummy-image"/>
-          <a href="" className='absolute top-1 ml-2 text-2xl font-semibold text-neutral-50 pt-6 pl-4'></a>
           <button className="services-item-btn">View More..</button>
       </div>
       
