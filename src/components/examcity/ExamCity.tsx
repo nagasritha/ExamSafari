190b6217facf717b3079ce37d393720a7d98e8b6
nagasritha <erchandrakalachandrakala665@gmail.com>
2024-01-25 23:07:31 +0530

    Committing local changes in ExamCity.tsx

--- src/components/examcity/ExamCity.tsx
+++ src/components/examcity/ExamCity.tsx
@@ -47,29 +47,32 @@ function ExamCity() {
     }
   };
   return (
-    <div className='services-container lg:my-26'>
+    <div className='services-container my-16'>
         <div className='service-heading'>Exam Cities</div>
         <div className='service-para'>Little text about Cities </div>
         <div className='services-items' ref={containerRef}>
             
             <Link to="/allahabad">
             <div className="services-item relative h-64 transition-all duration-300 hover:bg-gradient-to-t from-red-500 to-pink-500">
-                <img className="h-[300px] w-auto rounded-[25px] p-2 service-image" src="./images/allahabad.png" alt="dummy-image"/>
-                <button className="exams-item-btn">Allahabad..</button>
+                <img className="h-[300px] w-auto rounded-[25px] p-2 service-image" src="./public/images/allahabad.png" alt="dummy-image"/>
+                <a href="" className='absolute top-1 ml-2 text-2xl font-semibold text-neutral-50 pt-6 pl-4'></a>
+                <button className="absolute bottom-2 left-3 bg-red-500 text-white p-2 rounded-[15px] hover:bg-blue-600 m-2">Allahabad..</button>
             </div>
             </Link>
           
             <Link to="/lucknow">
             <div className="services-item relative h-64 transition-all duration-300 hover:bg-gradient-to-t from-red-500 to-pink-500">
-                <img className="h-[300px] w-auto rounded-[25px] p-2 service-image" src="./images/lucknow.png" alt="dummy-image"/>
-                <button className="exams-item-btn">Lucknow..</button>
+                <img className="h-[300px] w-auto rounded-[25px] p-2 service-image" src="./public/images/lucknow.png" alt="dummy-image"/>
+                <a href="" className='absolute top-1 ml-2 text-2xl font-semibold uppercase text-neutral-50 pt-6 pl-4'></a>
+            <button className="absolute bottom-2 left-3 bg-red-500 text-white p-2 rounded-[15px] hover:bg-blue-600 m-2">Lucknow..</button>
             </div>
             </Link>
 
             <Link to="/kanpur">
             <div className="services-item relative h-64 transition-all duration-300 hover:bg-gradient-to-t from-red-500 to-pink-500">
-                <img className="h-[300px] w-auto rounded-[25px] p-2 service-image" src="./images/kanpur.png" alt="dummy-image"/>
-                <button className="exams-item-btn">Kanpur..</button>
+                <img className="h-[300px] w-auto rounded-[25px] p-2 service-image" src="./public/images/kanpur.png" alt="dummy-image"/>
+                <a href="" className='absolute top-1 ml-2 text-2xl font-semibold text-neutral-50 pt-6 pl-4'></a>
+                <button className="absolute bottom-2 left-3 bg-red-500 text-white p-2 rounded-[15px] hover:bg-blue-600 m-2">Kanpur..</button>
             </div>
             </Link>
         </div>
@@ -83,24 +86,16 @@ function ExamCity() {
       </div>
       }
      </div>
+               
+      {/* Right arrow control */}
       <div>
-      {showLeftArrow && <div className="control-style"
-          onClick={scrollLeft}
-        >
-        &lt;
-        </div>
-        }
+      {showRightArrow && <div className="control-style"
+        onClick={scrollRight}>
+       &gt;
+      </div>}
       </div>
-                
-        {/* Right arrow control */}
-        <div>
-        {showRightArrow && <div className="control-style"
-          onClick={scrollRight}>
-        &gt;
-        </div>}
-        </div>
-        </div>
-  </div>
+      </div>
+</div>
   )
 }
 
